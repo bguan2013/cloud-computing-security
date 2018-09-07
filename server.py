95e5fd70023112c1f05f069aa7ae2ed3cdf018ee
bguan2013 <bguan@oswego.edu>
2018-09-07 14:43:33 -0400

    Version 1

--- server.py
+++ server.py
@@ -15,41 +15,49 @@ class Encoder(json.JSONEncoder):
 
 
 class Remove(webapp2.RequestHandler): 
-	def post(self):
-		event_title = self.request.get('event-id')
-		remove_event = Event.query().get()
-		remove_event.delete()
+	def delete(self, id):
+		event_id = 'Does not exist'
+		try:
+			path_array = self.request.path.split('/')
+			logging.info('path array: ' + repr(path_array))
+			if len(path_array) > 1:
+				event_id = path_array[len(path_array)-1]		
+				#Event.query(Event.Key.id() == event_id).fetch().delete()
+				ndb.Key(urlsafe=event_id).delete()
+				self.response.write(json.dumps(dict(id=repr(event_id), status='success')))
+		except Exception as e:
+			self.response.write(json.dumps(dict(id=repr(event_id), status='failed to delete event')))
+			logging.exception(e)
+
 
 class SubmitForm(webapp2.RequestHandler):
 	def post(self):
 		try:
-			event_title = self.request.get('title')
-			event_content = self.request.get('content')  
-			event_date = datetime.strptime(self.request.get('date'), '%Y-%m-%d')
+			json_event = json.loads(self.request.body)
+			event_title = json_event.get('title')
+			event_content = json_event.get('content')  
+			event_date = datetime.strptime(json_event.get('date'), '%Y-%m-%d')
 			event = Event(title=event_title, content=event_content, date=event_date)
 			event.put()
-			self.response.set_cookie('submit_status', 'Success')
-			self.response.set_cookie('event_id', repr(event.key.id()))
+			self.response.write(json.dumps(dict(id=repr(event.key.urlsafe()), status='success')))
 		except Exception as e: 
-			self.response.set_cookie('submit_status', 'Failed to save event')
+			self.response.write(json.dumps(dict(id=repr(event.key.urlsafe()), status='failed to save event')))
 			logging.exception(e)
-		self.redirect('/index.html')
-
 
 class AllEvents(webapp2.RequestHandler):
 	def date_handler(obj):
 	    if hasattr(obj, 'isoformat'):
 	        return obj.isoformat()
 	def get(self):
-		all_events = [e.to_dict() for e in Event.query_all().fetch()]
+		all_events = [dict(e.to_dict(), **dict(id=repr(e.key.urlsafe()))) for e in Event.query_all().fetch()]
 		all_events_json = json.dumps(all_events, cls=Encoder, indent=4)
 		self.response.headers['Content-Type'] = 'application/json'
 		self.response.out.write(all_events_json)
 
 app = webapp2.WSGIApplication([
-	('/event', SubmitForm),
 	('/events', AllEvents),
-	('/event/*', Remove)
+	('/event', SubmitForm),
+	('/event/(.*)', Remove)
 ], debug=True)
 
 
